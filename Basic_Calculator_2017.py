0f9b297f61869c76d9c783bff024aa47d0549f07
DownTheMatrix <33358858+DownTheMatrix@users.noreply.github.com>
2017-11-29 16:06:01 +0000

    Add files via upload

---
+++ Basic_Calculator_2017.py
@@ -0,0 +1,68 @@
+import time
+
+def welcome_alert():
+    print('''
+Welcome to Basic Calculator 2017!
+''')
+
+def calc():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+** for power
+% for modulo
+''')
+
+    number1 = int(input('Please enter your first number: '))
+    number2 = int(input('Please enter your second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number1, number2))
+        print(number1 + number2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number1, number2))
+        print(number1 - number2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number1, number2))
+        print(number1 * number2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number1, number2))
+        print(number1 / number2)
+
+    elif operation == '**':
+        print('{} ** {} = '.format(number1, number2))
+        print(number1 ** number2)
+
+    elif operation == '%':
+        print('{} % {} = '.format(number1, number2))
+        print(number1 % number2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+def again():
+    print("Do you want to perform another calculation? Y/N")
+    more = input("")
+    if more.lower() == "y":
+        time.sleep(0.5)
+        calc()
+    elif more.lower() == "n":
+        print("Thank you for using Basic Calculator 2017!")
+        time.sleep(1)
+        exit()
+    else:
+        print("Please enter Y for yes or N for no.")
+        time.sleep(1)
+        again()
+
+welcome_alert()
+calc()
+x = 0
+while True:
+    again()
